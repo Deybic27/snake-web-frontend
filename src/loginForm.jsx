e804ade1e0ba37330dbd1138f2951f57ca29415d
Paulaleon27 <paulaleonh99@gmail.com>
2026-01-30 00:15:13 -0500

    Api response validation Login form

--- src/loginForm.jsx
+++ src/loginForm.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form"
 
 function Login() {
@@ -8,25 +9,103 @@ function Login() {
     formState: { errors }
   } = useForm();
 
-  const entrar = (data) => {
-    console.log('entrar', data);
+  const [
+    apiError,
+    setApiError
+   ] = useState("");
+
+  const validar = async (data) => {
+    
+    var myHeaders = new Headers();
+    myHeaders.append("Accept", "application/json");
+    myHeaders.append("Content-Type", "application/json");
+
+    var url = 'http://127.0.0.1:8000/api/login';
+
+    const raw = JSON.stringify({
+      "username": data.username,
+      "password": data.password,
+    });
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow',
+      credentials: 'include'
+    }
+
+    fetch(url, requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      if(result.message == 'User not found.'){
+        setApiError("Usuario no registrado");
+        return;
+      }
+      if(result.message == 'Invalid credentials'){
+        setApiError("Contraseña incorrecta");
+        return;
+      }
+      if(result.message == 'User logged in successfully'){
+        console.log('USUARIO LOGUEADO: ', result);
+      }
+    })
+    .catch(error => {
+      console.log('ERROR', error);
+      setApiError("Error de conexión con el servidor");
+    });
+
+
   }
 
   return (
-    <form className="formContent" onSubmit={handleSubmit(entrar)}>
+    <form className="formContent" onSubmit={handleSubmit(validar)}>
       <div className="formContentInfo">
         {/*LABEL - INPUT*/}
         <div className="formContentInputs">
-          <label className="formLabel" htmlFor="usuario">Usuario*</label>
-          <input type="text" className="formInput" placeholder="Digite su usuario" {...register("usuario", { required: true })}/>
-          {errors.usuario && <p className="mgError">Campo requerido</p>}
+          <label className="formLabel" htmlFor="username">Usuario*</label>
+          <input type="text" className="formInput" id="username" placeholder="Digite su usuario"
+            {...register("username", {
+              required: {
+                value: true,
+                message: "Campo requerido"
+              },
+              pattern: {
+                value: /^[A-Za-z0-9._]+$/,
+                message: "El formato no es correcto"
+              }
+            })}
+            onChange={() => {
+              setApiError("");
+            }}
+          />
+          {errors.username && <span className="mgError">{errors.username.message}</span>}
         </div>
         {/*LABEL - INPUT*/}
         <div className="formContentInputs">
-          <label className="formLabel" htmlFor="contrasena">Contraseña*</label>
-          <input type="text" className="formInput" placeholder="Digite su contraseña" {...register("contrasena", { required: true })}/>
-          {errors.contrasena && <p className="mgError">Campo requerido</p>}
+          <label className="formLabel" htmlFor="password">Contraseña*</label>
+          <input type="password" className="formInput" id="password" placeholder="Digite su contraseña"
+            {...register("password", {
+              required: {
+                value: true,
+                message: "Campo requerido"
+              } ,
+              minLength: {
+                value: 8,
+                message: "Mínimo 8 caracteres"
+              } ,
+              maxLength: {
+                value: 100,
+                message: "Máximo 100 caracteres" 
+              }
+            })}
+            onChange={() => {
+              setApiError("");
+            }}
+          />
+          {errors.password && <span className="mgError">{errors.password.message}</span>}
         </div>
+        {apiError && <span className="mgError">{apiError}</span>}   
       </div>
       {/*BOTON ENTRAR*/}
       <div className="formContentBtn">

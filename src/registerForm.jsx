514f92d68e9ea3b1d022fe493a8b9bca99c14ce1
Paulaleon27 <paulaleonh99@gmail.com>
2026-01-30 01:40:24 -0500

    Api response validation Register form

--- src/registerForm.jsx
+++ src/registerForm.jsx
@@ -1,49 +1,170 @@
+import { useState } from "react"
 import { useForm } from "react-hook-form"
 
 function Register() {
 
+  const patterns = {
+    username: /^[A-Za-z0-9._]+$/,
+    email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+    password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&._#\-])[A-Za-z\d@$!%*?&._#\-]{8,100}$/,
+    confirmpassword: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&._#\-])[A-Za-z\d@$!%*?&._#\-]{8,100}$/
+  }
+
+  const messages = {
+    username: {
+      req: "Campo requerido",
+      invalid: "Debe incluir mínimo una letra o número"
+    },
+    email: {
+      req: "Campo requerido",
+      invalid: "Datos inválidos"
+    },
+    password: {
+      req: "Campo requerido",
+      invalid: "Debe incluir mínimo 8 dígitos, una mayúscula, una minúscula, un número y un carácter especial"
+
+    },
+    confirmpassword: {
+      req: "Campo requerido"
+    }
+  }
+
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors }
   } = useForm();
 
+  const [
+    apiError,
+    setApiError
+  ] = useState("");
+
+  const password = watch("password");
+
   const registrar = (data) => {
-    console.log('registrar', data);
+    var myHeaders = new Headers();
+    myHeaders.append("Accept", "application/json");
+    myHeaders.append("Content-Type", "application/json");
+
+    var url = 'http://127.0.0.1:8000/api/register';
+
+    const raw = JSON.stringify({
+      "username": data.username,
+      "email": data.email,
+      "password": data.password,
+      "password_confirmation": data.confirmpassword,
+    });
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow',
+      credentials: 'include'
+    }
+
+    fetch(url, requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      if(result.message == 'The username has already been taken.'){
+        setApiError("El usuario ya se encuentra registrado");
+        return;
+      }
+      if(result.message == 'The password field confirmation does not match.'){
+        setApiError("Las contraseñas no coinciden");
+        return;
+      }
+      if(result.message == 'The email has already been taken.'){
+        setApiError("El correo ya se encuentra registrado");
+        return;
+      }
+      if(result.message == 'User registered successfully'){
+        console.log('REGISTRADO', result);
+      }
+    })
+    .catch(error => {
+      console.log('ERROR', error);
+      setApiError("Error de conexión con el servidor");
+    });
   }
 
   return (
     <form className="formContent formContentReg" onSubmit={handleSubmit(registrar)}>
-        <div className="formContentInfo">
+      <div className="formContentInfo">
         {/*LABEL - INPUT*/}
         <div className="formContentInputs">
-            <label className="formLabel" htmlFor="usuario">Usuario*</label>
-            <input type="text" className="formInput" placeholder="Digite su usuario" {...register("usuario", { required: true })}/>
-            {errors.usuario && <p className="mgError">Campo requerido</p>}
+            <label className="formLabel" htmlFor="username">Usuario*</label>
+            <input type="text" className="formInput" id="username" placeholder="Digite su usuario" 
+              {...register("username", {
+                required: {
+                  value: true,
+                  message: messages.username.req
+                },
+                pattern: {
+                  value: patterns.username,
+                  message: messages.username.invalid
+                }
+              })}
+            />
+            {errors.username && <p className="mgError">{errors.username.message}</p>}
         </div>
         {/*LABEL - INPUT*/}
         <div className="formContentInputs">
-            <label className="formLabel" htmlFor="email">Email*</label>
-            <input type="email" className="formInput" placeholder="Digite su email" {...register("email", { required: true })}/>
-            {errors.email && <p className="mgError">Campo requerido</p>}
+          <label className="formLabel" htmlFor="email">Email*</label>
+          <input type="email" className="formInput" id="email" placeholder="Digite su email"
+            {...register("email", {
+              required: {
+                value: true,
+                message: messages.email.req
+              },
+              pattern: {
+                value: patterns.email,
+                message: messages.email.invalid
+              }
+            })}
+          />
+          {errors.email && <p className="mgError">{errors.email.message}</p>}
         </div>
         {/*LABEL - INPUT*/}
         <div className="formContentInputs">
-            <label className="formLabel" htmlFor="contrasena">Contraseña*</label>
-            <input type="password" className="formInput" placeholder="Digite su contrasena" {...register("contrasena", { required: true })}/>
-            {errors.contrasena && <p className="mgError">Campo requerido</p>}
+          <label className="formLabel" htmlFor="password">Contraseña*</label>
+          <input type="password" className="formInput" id="password" placeholder="Digite su contrasena"
+            {...register("password", {
+              required: {
+                value: true,
+                message: messages.password.req
+              },
+              pattern: {
+                value: patterns.password,
+                message: messages.password.invalid
+              }
+            })}
+          />
+          {errors.password && <p className="mgError">{errors.password.message}</p>}
         </div>
         {/*LABEL - INPUT*/}
         <div className="formContentInputs">
-            <label className="formLabel" htmlFor="confirmarContraseña">Confirmar Contraseña*</label>
-            <input type="password" className="formInput" placeholder="Confirme su contrasena" {...register("confirmarContraseña", { required: true })}/>
-            {errors.confirmarContraseña && <p className="mgError">Campo requerido</p>}
+          <label className="formLabel" htmlFor="confirmpassword">Confirmar Contraseña*</label>
+          <input type="password" className="formInput" id="confirmpassword" placeholder="Confirme su contrasena"
+            {...register("confirmpassword", {
+              required: {
+                value: true,
+                message: messages.confirmpassword.req
+              },
+              validate: value =>
+                value === password || "Las contraseñas no coinciden"
+            })}
+          />
+          {errors.confirmpassword && <p className="mgError">{errors.confirmpassword.message}</p>}
         </div>
-        </div>
-        {/*BOTON ENTRAR*/}
-        <div className="formContentBtn">
-        <button className="btn btnGreen">Registrar</button>
-        </div>
+        {apiError && <span className="mgError">{apiError}</span>} 
+      </div>
+      {/*BOTON ENTRAR*/}
+      <div className="formContentBtn">
+        <button type="submit" className="btn btnGreen">Registrar</button>
+      </div>
     </form>
   )
 }
